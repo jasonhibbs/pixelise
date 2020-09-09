8cb3e619d7db67343af84f05bc6ba7320d3e60da
Jason Hibbs <jason@jasonhibbs.co.uk>
2020-09-09 17:20:22 +0100

    add PWA to config

--- vue.config.js
+++ vue.config.js
@@ -15,4 +15,22 @@ module.exports = {
     },
     sourceMap: true,
   },
+  pwa: {
+    name: 'Pixelise',
+    themeColor: '#fff',
+    appleMobileWebAppCapable: 'yes',
+    iconPaths: {
+      appleTouchIcon: 'img/icons/apple-touch-icon-precomposed.png',
+      msTileImage: 'img/icons/mstile-150x150.png',
+    },
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/serviceWorker.js',
+    },
+    msTileColor: '#4400ff',
+    manifestOptions: {
+      background_color: '#fff',
+      theme_color: 'transparent',
+    },
+  },
 }
