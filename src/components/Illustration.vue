741f0cd490a6e915faf70fa95a208f4ca1866127
Jason Hibbs <jason@jasonhibbs.co.uk>
2020-09-21 17:21:56 +0100

    animate illustration

--- src/components/Illustration.vue
+++ src/components/Illustration.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 
-  figure.illustration
+  figure.illustration(:style="styles")
     inline-svg(:src="src")
 
 </template>
@@ -15,6 +15,31 @@ export default class Illustration extends Vue {
   get src() {
     return `./img/svg/illustration.svg`
   }
+
+  tick = 0
+  tickInterval = 0
+
+  mounted() {
+    this.tickInterval = setInterval(() => {
+      this.tick++
+    }, 1600)
+  }
+
+  getRandomColor() {
+    const l = ~~(Math.random() * 80) + 0
+    return `hsl(0, 0%, ${l}%)`
+  }
+
+  get styles() {
+    const tick = this.tick
+    const pixels: any = {}
+
+    for (let i = 0; i < 15; i++) {
+      pixels[`--color-pixel-${i + 1}`] = this.getRandomColor()
+    }
+
+    return pixels
+  }
 }
 </script>
 <style lang="scss">
@@ -25,5 +50,24 @@ export default class Illustration extends Vue {
     position: relative;
     display: block;
   }
+
+  #pixels path {
+    transition: opacity 400ms, fill 400ms linear;
+  }
+
+  @for $i from 1 through 15 {
+    #pixels path:nth-child(#{$i}) {
+      fill: var(--color-pixel-#{$i});
+    }
+  }
+
+  #pixels:hover path {
+    opacity: 0.4;
+    fill: black;
+    transition: opacity 0.2s, fill 200ms;
+  }
+}
+
+@keyframes {
 }
 </style>
